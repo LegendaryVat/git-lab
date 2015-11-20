4dfd75567a3c22951f243a271b8325ea584159f0
fares alfowzan <bofozan2008@hotmail.com>
2015-11-19 21:19:22 -0800

    firstcommit

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 int add(int,int);
 
-int subtract(int,int);
-
+int subtract(int,int)
+{
+	int total;
+	total = a - b;
+	return total;
+}
 int main(){
+
 	
 	cout<<add(5,3)<<endl;
 	cout<<subtract(5,3)<<endl;
